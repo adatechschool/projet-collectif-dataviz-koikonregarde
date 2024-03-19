bd61b811cb909d0e6c7c21b737bd4e40d70154f2
AramirRp <antoinerp@gmail.com>
2024-03-19 15:10:39 +0100

    on a reset pour chaque film

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 ///////////////////////
-
+//initialisation de notre KEY et site de base
 const API_KEY = '3753c6e85bd59d368a602fd60fc98864';
 const BASE_URL = 'https://api.themoviedb.org/3/';
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
@@ -13,50 +13,58 @@ const options = {
 };
 
 ////////////////////////
-
+//écoute du bouton pour lancer le premier film aleatoire
 document.querySelector('#random-genre').addEventListener('click', async () => {
   try {
     let response = await fetch(BASE_URL + 'genre/movie/list?api_key=' + API_KEY, options);
     let data = await response.json();
+
     let randomGenre = data.genres[Math.floor(Math.random() * data.genres.length)];
     let genreMovies = await fetchMoviesByGenre(randomGenre.id);
+
     console.log(genreMovies);
     const movie = genreMovies[0];
+
     document.getElementById('movieJacket').src = IMG_URL + movie.poster_path;
     document.getElementById('movieName').textContent = movie.title;
     document.getElementById('movieResume').textContent = movie.overview;
 
-    let movieCredits = await fetchMovieCredits(movie.id);
+    const movieCredits = await fetchMovieCredits(movie.id);
     console.log(movieCredits);
+
     const cast = movieCredits.cast;
     const crew = movieCredits.crew;
+
     console.log(cast);
     console.log(crew);
-    for(let i=0; i<5;i++){
-      let cast2 = cast[i];
-      cast2 = cast2.name;
-      document.getElementById('cast').textContent += cast2 + ', ' ;
+    let actors = [];
 
+    for(let i=0; i<5;i++){
+     let cast2 = cast[i];
+    actors.push(' ' + cast2.name);
+     console.log(actors);
     }
+    document.getElementById('cast').textContent = 'Cast by : ' + actors ;
+
 
     for(let i=0; i<crew.length; i++){
       let crew2 = crew[i];
-      console.log(crew2.job);
       if(crew2.job == "Director"){
-        console.log("ping")
         crew2 = crew2.name;
-        document.getElementById('movieDirector').textContent += crew2 + ', ' ;
+        document.getElementById('movieDirector').textContent = 'directed by : ' + crew2 ;
       }
-    }
 
+    }
 
     document.querySelector('#stream').addEventListener('click', async () => {
     let url = `https://movie-web-me.vercel.app/#/media/tmdb-movie-${movie.id}`
-    document.querySelector('#stream').setAttribute('href',url);
+    document.querySelector('#stream').setAttribute('href',url); 
   })
+
   } catch (err) {
     console.error(err);
   }
+
 });
 
 async function fetchMoviesByGenre(genreId) {
