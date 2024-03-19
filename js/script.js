51c34b75575c1c71714853728e8f4785f659fd31
AramirRp <antoinerp@gmail.com>
2024-03-19 12:27:24 +0100

    commit RANDOM IS WURRKINNN

--- js/script.js
+++ js/script.js
@@ -1,61 +1,42 @@
-const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNzUzYzZlODViZDU5ZDM2OGE2MDJmZDYwZmM5ODg2NCIsInN1YiI6IjY1Zjg1N2QwYWUzODQzMDE2NDRhZTc5ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.aVB-ASkpiHWzORGjq227KnZqgpU5M_D92tkSE9fOsbo'
-    }
-  };
-  
-  fetch('https://api.themoviedb.org/3/authentication', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+///////////////////////
 
-//CLE API
-const API_KEY='3753c6e85bd59d368a602fd60fc98864';
-//URL ORIGINELLE
+const API_KEY = '3753c6e85bd59d368a602fd60fc98864';
 const BASE_URL = 'https://api.themoviedb.org/3/';
-//URL AVEC TON API
-const API_URL = BASE_URL + 'discover/movie?sort_by=top-rated&' + API_KEY ;
-//URL DES IMAGES
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 
-const input = document.querySelector('.search input');
-const btn = document.querySelector('.search button');
-const main_title = document.querySelector('.movie');
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNzUzYzZlODViZDU5ZDM2OGE2MDJmZDYwZmM5ODg2NCIsInN1YiI6IjY1Zjg1N2QwYWUzODQzMDE2NDRhZTc5ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.aVB-ASkpiHWzORGjq227KnZqgpU5M_D92tkSE9fOsbo'
+  }
+};
 
-async function get_movie_by_search (search_term) {
-    const resp = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${search_term}`);
-    console.log(resp);
-    const respData = await resp.json();
-    return respData.results;
+////////////////////////
+
+document.querySelector('#random-genre').addEventListener('click', async () => {
+  try {
+    let response = await fetch(BASE_URL + 'genre/movie/list?api_key=' + API_KEY, options);
+    let data = await response.json();
+    let randomGenre = data.genres[Math.floor(Math.random() * data.genres.length)];
+    let genreMovies = await fetchMoviesByGenre(randomGenre.id);
+    console.log(genreMovies);
+    const movie = genreMovies[0];
+    document.getElementById('movieJacket').src = IMG_URL + movie.poster_path;
+    document.getElementById('movieName').textContent = movie.title;
+    document.getElementById('movieResume').textContent = movie.overview;
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+async function fetchMoviesByGenre(genreId) {
+  let minResult = 1;
+  let maxResult = 20;
+  minResult = Math.ceil(minResult);
+  maxResult = Math.floor(maxResult);
+  let randomNumb = Math.floor(Math.random() * (maxResult - minResult +1)) + minResult;
+  let response = await fetch(BASE_URL + 'discover/movie?api_key=' + API_KEY + '&with_genres=' + genreId + '&sort_by=top-rated.desc&page='+ randomNumb);
+  let data = await response.json();
+  return data.results.slice(0,1);
 }
-
-btn.addEventListener('click', add_searched_movie_to_dom);
-
-async function add_searched_movie_to_dom(){
-    const data = await get_movie_by_search(input.value)
-    console.log(data);
-
-}
-
-
-
-// == By Search ==
-// https://api.themoviedb.org/3/search/movie?api_key=here-goes-api-key&query=here-goes-seach-term
-
-// == By ID ==
-// https://api.themoviedb.org/3/movie/${id}?api_key=here-goes-api-key
-// The /movie/latest method holds the id of the latest movie which at the time of me writing this is currently sitting at 373816.
-
-// == Movie Trailer ==
-// https://api.themoviedb.org/3/movie/${id}/videos?api_key=here-goes-api-key
-
-// == Trending Movies ==
-// https://api.themoviedb.org/3/trending/all/day?api_key=here-goes-api-key
-
-// == Iframe for youtube video ==
-// <iframe width="560" height="315" src="https://www.youtube.com/embed/here-goes-movie-path-trailer" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
-// == Example Video ==
-// https://www.youtube.com/watch?v=Kmo8NLKkfcQ
