4dc41ec47d2d718a6126efe4e66ee291ede5a1f7
AramirRp <antoinerp@gmail.com>
2024-03-19 16:38:06 +0100

    translation FR + notes

--- js/script.js
+++ js/script.js
@@ -18,36 +18,61 @@ document.querySelector('#random-genre').addEventListener('click', async () => {
   try {
     let response = await fetch(BASE_URL + 'genre/movie/list?api_key=' + API_KEY, options);
     let data = await response.json();
-
     let randomGenre = data.genres[Math.floor(Math.random() * data.genres.length)];
+    //on récupère le film aléatoire
     let genreMovies = await fetchMoviesByGenre(randomGenre.id);
-
-    console.log(genreMovies);
     const movie = genreMovies[0];
     const year = movie.release_date.slice(0, 4);
 
-    console.log(year);
-    document.getElementById('movieJacket').src = IMG_URL + movie.poster_path;
-    document.getElementById('movieName').textContent = movie.title;
-    document.getElementById('movieResume').textContent = movie.overview;
-    document.getElementById('movieYear').textContent = year;
-    const movieCredits = await fetchMovieCredits(movie.id);
-    console.log(movieCredits);
+    //on récupère les traductions possibles du film
+    let languageMovie = await fetchMovieLanguages(movie.id);
+    let language = languageMovie.translations;
+    //on créé la boucle pour trouver la langue FR
+    for(let i=0; i<language.length; i++){ 
+      let language2 = language[i];
+      //si langue FR trouvée, alors on va prendre le titre et le résumé du film
+      if(language2.iso_3166_1 == "FR"){
+      
+        let languageTitle = language2.data;
+        languageTitle=languageTitle.title;
+        let languageOverview = language2.data;
+        languageOverview=languageOverview.overview;
 
+      //si le titre ou le synopsis sont vide, alors on va afficher le titre ou le résumé original
+      if(languageTitle.length === 0){
+        document.getElementById('movieName').textContent = movie.title;
+      }else{
+        document.getElementById('movieName').textContent = languageTitle;
+      } 
+      if(languageOverview.length === 0){
+        document.getElementById('movieResume').textContent = movie.overview;
+      }else{
+        document.getElementById('movieResume').textContent = languageOverview;
+      }
+        break
+      //si pas de langue FR trouvée, on va alor afficher normalement en original
+      }else{
+        document.getElementById('movieName').textContent = movie.title;
+        document.getElementById('movieResume').textContent = movie.overview;
+      }
+    }
+    //on affiche le poster et l'année de sortie
+    document.getElementById('movieJacket').src = IMG_URL + movie.poster_path;
+    document.getElementById('movieYear').textContent = year;
+
+    //on récupère les informations de casting et d'équipe
+    const movieCredits = await fetchMovieCredits(movie.id);
     const cast = movieCredits.cast;
     const crew = movieCredits.crew;
-
-    console.log(cast);
-    console.log(crew);
     let actors = [];
-
+    //on boucle pour trouver les 5 premiers acteurs du film et les afficher
     for(let i=0; i<5;i++){
      let cast2 = cast[i];
     actors.push(' ' + cast2.name);
     }
     document.getElementById('cast').textContent = 'Cast by : ' + actors ;
 
-
+    //on boucle pour trouver le réalisateur du film et l'afficher
     for(let i=0; i<crew.length; i++){
       let crew2 = crew[i];
       if(crew2.job == "Director"){
@@ -56,18 +81,20 @@ document.querySelector('#random-genre').addEventListener('click', async () => {
       }
 
     }
-
+    //on créé le lien pour le bouton pour rediriger vers la plateforme de streaming movie-web
     document.querySelector('#stream').addEventListener('click', async () => {
     let url = `https://movie-web-me.vercel.app/#/media/tmdb-movie-${movie.id}`
     document.querySelector('#stream').setAttribute('href',url); 
   })
-
+  //on gère les erreurs de base
   } catch (err) {
     console.error(err);
   }
 
 });
 
+
+//fonction qui va chercher un film aléatoirement
 async function fetchMoviesByGenre(genreId) {
   let minResult = 1;
   let maxResult = 50;
@@ -79,15 +106,18 @@ async function fetchMoviesByGenre(genreId) {
   return data.results.slice(0,1);
 }
 
+
+//fonction qui va chercher les crédits du film
 async function fetchMovieCredits(movieId){
   let response = await fetch(`https://api.themoviedb.org/3/movie/${movieId}/credits?language=en-US`, options)
   let data = await response.json();
   return data;
 }
-  // fetch(`https://api.themoviedb.org/3/movie/${movieId}/credits?language=en-US`, options)
-  // .then(response => response.json())
-  // .then(response => console.log(response))
-  // let responseCredits = await fetch(`https://api.themoviedb.org/3/movie/${movie.id}/credits?language=en-US`, options);
-  // let dataCredits = await response.json();
-  // console.log(dataCredits);
 
+
+//fonction qui va chercher les traductions du film
+async function fetchMovieLanguages(movieId){
+  let response = await fetch(`https://api.themoviedb.org/3/movie/${movieId}/translations`, options)
+  let data = await response.json();
+  return data;
+}
